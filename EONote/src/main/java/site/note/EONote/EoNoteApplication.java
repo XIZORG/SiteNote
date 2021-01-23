804a0f0e8fbdb277bbd571ce61eff0e8563cd9d0
XIZORG <leo.jek7@gmail.com>
2021-01-23 19:51:54 +0200

    f
    Add Java app

---
+++ EONote/src/main/java/site/note/EONote/EoNoteApplication.java
@@ -0,0 +1,13 @@
+package site.note.EONote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoNoteApplication.class, args);
+	}
+
+}
---
+++ EONote/src/test/java/site/note/EONote/EoNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package site.note.EONote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
